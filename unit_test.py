2174ef9553d58264e47fc79ffeb25abe7d84c37a
Mandam Nitin Kumar Reddy (Digital) <MA391643@wipro.com>
2018-04-25 16:14:59 +0530

    Updated automation script to navigate across the application modules in SECAT

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,10 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class PythonOrgSearch(unittest.TestCase):
 
@@ -9,15 +13,40 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_search_in_python_org(self):
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found" not in driver.page_source
+        driver.get("http://localhost:9000/")
+        self.assertIn("Telstra Software Engineering Capability Assessment Tool", driver.title)
 
-    # def tearDown(self):
-    #     self.driver.quit()
+        # Code to open modal pop up
+        self.open_login_pop_up()
+
+        # Code to fill in user name and password
+        self.on_click_fields("login-username", "guest")
+        self.on_click_fields("login-password", "password")
+
+        # Submit after details are filled
+        driver.find_element(By.XPATH, '//button[contains(text(), "Go")]').click()
+
+        self.go_to_team_responses("team-responses")
+        self.go_to_team_responses("team-profiles")
+        self.go_to_team_responses("hpse-metrics")
+        self.go_to_team_responses("health-metrics")
+
+    def on_click_fields(self, key_field, value_field):
+        driver = self.driver
+        userName = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, key_field)))
+        userName.send_keys(value_field)
+
+    def open_login_pop_up(self):
+        driver = self.driver
+        elemJumpin = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "jump-in-btn")))
+        elemJumpin.click()
+        driver.implicitly_wait(20)
+
+    def go_to_team_responses(self, class_name):
+        driver = self.driver
+        teamResponse = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, class_name)))
+        teamResponse.click()
+        time.sleep(20)
 
 if __name__ == "__main__":
     unittest.main()
